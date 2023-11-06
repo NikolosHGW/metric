2f96d5b94cc46e7f8531fdd57ae3f35db5a7af9d
Nikita Bakarev <nic231731@yandex.ru>
2023-11-06 15:40:03 +0300

    feat: init metrics map

--- internal/client/metrics/metrics.go
+++ internal/client/metrics/metrics.go
@@ -10,7 +10,7 @@ type ClientMetrics interface {
 	IncPollCount()
 	UpdateRandomValue()
 	RefreshMetrics()
-	GetMetrics() []interface{}
+	GetMetrics() map[string]interface{}
 }
 
 type Metrics struct {
@@ -31,8 +31,38 @@ func (m *Metrics) RefreshMetrics() {
 	runtime.ReadMemStats(&m.MemStats)
 }
 
-func (m Metrics) GetMetrics() []interface{} {
-	return []interface{}{m.Alloc, m.PollCount, m.RandomValue}
+func (m Metrics) GetMetrics() map[string]interface{} {
+	return map[string]interface{}{
+		"Alloc":         m.Alloc,
+		"BuckHashSys":   m.BuckHashSys,
+		"Frees":         m.Frees,
+		"GCCPUFraction": m.GCCPUFraction,
+		"GCSys":         m.GCSys,
+		"HeapAlloc":     m.HeapAlloc,
+		"HeapIdle":      m.HeapIdle,
+		"HeapInuse":     m.HeapInuse,
+		"HeapObjects":   m.HeapObjects,
+		"HeapReleased":  m.HeapReleased,
+		"HeapSys":       m.HeapSys,
+		"LastGC":        m.LastGC,
+		"Lookups":       m.Lookups,
+		"MCacheInuse":   m.MCacheInuse,
+		"MCacheSys":     m.MCacheSys,
+		"MSpanInuse":    m.MSpanInuse,
+		"MSpanSys":      m.MSpanSys,
+		"Mallocs":       m.Mallocs,
+		"NextGC":        m.NextGC,
+		"NumForcedGC":   m.NumForcedGC,
+		"NumGC":         m.NumGC,
+		"OtherSys":      m.OtherSys,
+		"PauseTotalNs":  m.PauseTotalNs,
+		"StackInuse":    m.StackInuse,
+		"StackSys":      m.StackSys,
+		"Sys":           m.Sys,
+		"TotalAlloc":    m.TotalAlloc,
+		"PollCount":     m.PollCount,
+		"RandomValue":   m.RandomValue,
+	}
 }
 
 func NewMetrics() *Metrics {
