df0096a5de6c9bc85111cf8160513f96536860db
Nikita Bakarev <nic231731@yandex.ru>
2023-12-07 08:03:37 +0300

    feat: create logger

--- internal/server/middlewares/middlewares.go
+++ internal/server/middlewares/middlewares.go
@@ -3,9 +3,64 @@ package middlewares
 import (
 	"net/http"
 	"strings"
+	"time"
 
+	"github.com/NikolosHGW/metric/internal/server/logger"
 	"github.com/NikolosHGW/metric/internal/util"
 )
+
+type (
+	responseData struct {
+		status int
+		size   int
+	}
+
+	loggingResponseWriter struct {
+		http.ResponseWriter
+		responseData *responseData
+	}
+)
+
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	size, err := r.ResponseWriter.Write(b)
+	r.responseData.size += size
+	return size, err
+}
+
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	r.ResponseWriter.WriteHeader(statusCode)
+	r.responseData.status = statusCode
+}
+
+func WithLogging(h http.Handler) http.Handler {
+	logFn := func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
+
+		lw := loggingResponseWriter{
+			ResponseWriter: w,
+			responseData:   responseData,
+		}
+
+		h.ServeHTTP(&lw, r)
+
+		duration := time.Since(start)
+
+		logger.Log.Sugar().Infoln(
+			"uri", r.RequestURI,
+			"method", r.Method,
+			"duration", duration,
+			"status", responseData.status,
+			"size", responseData.size,
+		)
+	}
+
+	return http.HandlerFunc(logFn)
+}
 
 func CheckMetricNameMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
--- internal/server/routes/routes.go
+++ internal/server/routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"net/http"
 
+	"github.com/NikolosHGW/metric/internal/server/middlewares"
 	"github.com/NikolosHGW/metric/internal/server/routes/update"
 	"github.com/NikolosHGW/metric/internal/server/routes/value"
 	"github.com/go-chi/chi"
@@ -16,6 +17,8 @@ type Handler interface {
 
 func InitRouter(handler Handler) *chi.Mux {
 	r := chi.NewRouter()
+
+	r.Use(middlewares.WithLogging)
 
 	r.Route("/", func(r chi.Router) {
 		r.Get("/", handler.GetMetrics)
