46312092dff6e02c30f0045a58d551639756dd49
Nikita Bakarev <nic231731@yandex.ru>
2024-02-29 07:47:58 +0300

    feat: set and get metric db

--- internal/server/storage/db.go
+++ internal/server/storage/db.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/jmoiron/sqlx"
+	"go.uber.org/zap"
 
 	"github.com/NikolosHGW/metric/internal/models"
 )
@@ -18,16 +19,50 @@ import (
 // 	GetAllMetrics(context.Context) []string
 // }
 
-func NewDBStorage(sql *sqlx.DB) *DBStorage {
+func NewDBStorage(sql *sqlx.DB, log customLogger) *DBStorage {
 	return &DBStorage{
 		sql: sql,
+		log: log,
 	}
 }
 
 type DBStorage struct {
 	sql *sqlx.DB
+	log customLogger
 }
 
 func (ds DBStorage) SetMetric(m models.Metrics, ctx context.Context) {
-	ds.sql.ExecContext(ctx, "INSERT INTO metrics (country, telcode) VALUES (?, ?)")
+	_, err := ds.sql.ExecContext(
+		ctx,
+		`INSERT INTO metrics (id, type, delta, value)
+		VALUES ($1, $2, $3, $4)
+		ON CONFLICT (id) DO UPDATE SET type = EXCLUDED.type, 
+			delta = EXCLUDED.delta, 
+			value = EXCLUDED.value`,
+		m.ID,
+		m.MType,
+		m.Delta,
+		m.Value,
+	)
+	if err != nil {
+		ds.log.Info("cannot exec set metric", zap.Error(err))
+	}
+}
+
+func (ds DBStorage) GetMetric(name string, ctx context.Context) (models.Metrics, error) {
+	row := ds.sql.QueryRowxContext(ctx, "SELECT id, type, delta, value FROM metrics WHERE id = &1", name)
+
+	model := models.Metrics{}
+
+	err := row.Scan(&model.ID, &model.MType, &model.Delta, &model.Value)
+
+	if err != nil {
+		ds.log.Info("cannot scan row when getting metric", zap.Error(err))
+	}
+
+	return model, err
+}
+
+func (ds DBStorage) SetGaugeMetric(name string, value models.Gauge, ctx context.Context) {
+
 }
--- internal/server/storage/memory.go
+++ internal/server/storage/memory.go
@@ -17,7 +17,7 @@ type MemStorage struct {
 	metrics map[string]metricValue
 }
 
-func (ms MemStorage) GetGaugeMetric(name string, _ctx context.Context) (models.Gauge, error) {
+func (ms MemStorage) GetGaugeMetric(name string, _ context.Context) (models.Gauge, error) {
 	metric, exist := ms.metrics[name]
 	if exist {
 		return metric.gauge, nil
@@ -26,7 +26,7 @@ func (ms MemStorage) GetGaugeMetric(name string, _ctx context.Context) (models.G
 	return 0, fmt.Errorf("gauge metric %s not found", name)
 }
 
-func (ms MemStorage) GetCounterMetric(name string, _ctx context.Context) (models.Counter, error) {
+func (ms MemStorage) GetCounterMetric(name string, _ context.Context) (models.Counter, error) {
 	metric, exist := ms.metrics[name]
 	if exist {
 		return metric.counter, nil
@@ -35,7 +35,7 @@ func (ms MemStorage) GetCounterMetric(name string, _ctx context.Context) (models
 	return 0, fmt.Errorf("counter metric %s not found", name)
 }
 
-func (ms *MemStorage) SetGaugeMetric(name string, value models.Gauge, _ctx context.Context) {
+func (ms *MemStorage) SetGaugeMetric(name string, value models.Gauge, _ context.Context) {
 	metric, exist := ms.metrics[name]
 	if exist {
 		metric.gauge = value
@@ -50,7 +50,7 @@ func (ms *MemStorage) SetGaugeMetric(name string, value models.Gauge, _ctx conte
 	}
 }
 
-func (ms *MemStorage) SetCounterMetric(name string, value models.Counter, _ctx context.Context) {
+func (ms *MemStorage) SetCounterMetric(name string, value models.Counter, _ context.Context) {
 	metric, exist := ms.metrics[name]
 	if exist {
 		metric.counter += value
@@ -65,17 +65,17 @@ func (ms *MemStorage) SetCounterMetric(name string, value models.Counter, _ctx c
 	}
 }
 
-func (ms *MemStorage) SetMetric(m models.Metrics, _ctx context.Context) {
+func (ms *MemStorage) SetMetric(m models.Metrics, ctx context.Context) {
 	if m.MType == models.CounterType {
-		ms.SetCounterMetric(m.ID, models.Counter(*m.Delta), _ctx)
+		ms.SetCounterMetric(m.ID, models.Counter(*m.Delta), ctx)
 
 		return
 	}
 
-	ms.SetGaugeMetric(m.ID, models.Gauge(*m.Value), _ctx)
+	ms.SetGaugeMetric(m.ID, models.Gauge(*m.Value), ctx)
 }
 
-func getMetricsModel(name string, metric metricValue, _ctx context.Context) models.Metrics {
+func getMetricsModel(name string, metric metricValue, _ context.Context) models.Metrics {
 	if metric.counter != 0 {
 		return models.Metrics{ID: name, MType: models.CounterType, Delta: (*int64)(&metric.counter)}
 	}
@@ -83,26 +83,26 @@ func getMetricsModel(name string, metric metricValue, _ctx context.Context) mode
 	return models.Metrics{ID: name, MType: models.GaugeType, Value: (*float64)(&metric.gauge)}
 }
 
-func (ms *MemStorage) GetMetric(name string, _ctx context.Context) (models.Metrics, error) {
+func (ms *MemStorage) GetMetric(name string, ctx context.Context) (models.Metrics, error) {
 	metric, exist := ms.metrics[name]
 	if exist {
-		return getMetricsModel(name, metric, _ctx), nil
+		return getMetricsModel(name, metric, ctx), nil
 	}
 
 	return models.Metrics{}, fmt.Errorf("%s metric not found", name)
 }
 
-func (ms *MemStorage) GetMetricsModels(_ctx context.Context) []models.Metrics {
+func (ms *MemStorage) GetMetricsModels(ctx context.Context) []models.Metrics {
 	models := make([]models.Metrics, 0, len(ms.metrics))
 	for k := range ms.metrics {
-		model, _ := ms.GetMetric(k, _ctx)
+		model, _ := ms.GetMetric(k, ctx)
 		models = append(models, model)
 	}
 
 	return models
 }
 
-func (ms MemStorage) GetAllMetrics(_ctx context.Context) []string {
+func (ms MemStorage) GetAllMetrics(_ context.Context) []string {
 	result := make([]string, len(ms.metrics))
 
 	keys := make([]string, 0, len(ms.metrics))
