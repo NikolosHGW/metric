9564ba29fdc018d8ab8cc073a8128b0990facdac
Nikita Bakarev <nic231731@yandex.ru>
2024-03-14 07:14:54 +0300

    fix: change on BIGINT delta

--- internal/server/db/migrations/000001_create_metrics_table.up.sql
+++ internal/server/db/migrations/000001_create_metrics_table.up.sql
@@ -3,7 +3,7 @@ BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS metrics(
    id varchar PRIMARY KEY,
    type VARCHAR (50) NOT NULL,
-   delta INTEGER NULL,
+   delta BIGINT NULL,
    value DOUBLE PRECISION NULL
 );
 
