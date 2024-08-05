c0b7b99dbd89f4a6c55ed2ca7b4dbea97b05c19d
Nikita Bakarev <nic231731@yandex.ru>
2024-08-05 08:53:32 +0300

    feat: create grace shutdown for agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/NikolosHGW/metric/internal/client/config"
@@ -23,11 +27,20 @@ func main() {
 	stats := metrics.NewMetrics()
 
 	pollTicker := time.NewTicker(time.Duration(config.GetPollInterval()) * time.Second)
+	defer pollTicker.Stop()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
 	go func() {
-		for range pollTicker.C {
-			stats.CollectMetrics()
-			stats.CollectAdvancedMetric()
+		for {
+			select {
+			case <-pollTicker.C:
+				stats.CollectMetrics()
+				stats.CollectAdvancedMetric()
+			case <-ctx.Done():
+				return
+			}
 		}
 	}()
 
@@ -36,13 +49,19 @@ func main() {
 	requests := make(chan struct{}, rateLimit)
 
 	reportTicker := time.NewTicker(time.Duration(config.GetReportInterval()) * time.Second)
+	defer reportTicker.Stop()
 
 	for i := 0; i < rateLimit; i++ {
 		go func() {
-			for range reportTicker.C {
-				requests <- struct{}{}
-				request.SendBatchJSONMetrics(stats, config.GetAddress(), config.GetKey(), config.GetCryptoKeyPath())
-				<-requests
+			for {
+				select {
+				case <-reportTicker.C:
+					requests <- struct{}{}
+					request.SendBatchJSONMetrics(stats, config.GetAddress(), config.GetKey(), config.GetCryptoKeyPath())
+					<-requests
+				case <-ctx.Done():
+					return
+				}
 			}
 		}()
 	}
@@ -53,5 +72,14 @@ func main() {
 		"Build commit: ", buildCommit,
 	)
 
-	select {}
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+
+	sig := <-signalChan
+	fmt.Println("Received signal:", sig)
+	cancel()
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Agent exited gracefully")
 }
